7139742c2e57d6d9750a94f13560fb6eb5d4a077
ikealmighty <isaacyates7@gmail.com>
2025-02-02 13:03:37 -0600

    started implemeting auth middleware

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ import tagRoutes from './api/tags.js';
 import userRoutes from './api/users.js'
 import express from 'express';
 
+import auth from '../utils/auth.js'
+
 const router = express.Router();
 
-router.use('/api/events', eventRoutes);
-router.use('/api/tags', tagRoutes);
-router.use('/api/users', userRoutes);
+router.use('/api/events', auth, eventRoutes);
+router.use('/api/tags', auth, tagRoutes);
+router.use('/api/users', auth, userRoutes);
 
 export default router;
---
+++ utils/auth.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const { connectToDatabase } = require('../db/connection');
+
+// Middleware function to authenticate the user
+const authenticate = (req, res, next) => {
+    const token = req.header('Authorization'); // Get token from Authorization header
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify the token
+        req.user = decoded; // Attach the decoded user data to the request object
+        next(); // Proceed to the next middleware/route handler
+    } catch (err) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+};
+
+const authorizeUser = () => async (req, res, next) => {
+    if (!req.user) {
+        // TODO: redirect to login:
+    }
+
+    // grab the subscriptions for the user: FIXME: use projection to only get subscriptions
+    const db = await connectToDatabase();
+    const userData = await db.collection('users').findOne({ _id: req.user._id });
+    const { subscriptions } = userData;
+
+    // determine whether the current user logged in 
+    // has access to the url being hit up in this request
+
+    // users are authorized to access tag urls that they are subscribed to
+    // and they are authorized to access event urls that contain tags in their
+    // subscription list:
+
+
+
+    next();
+
+
+}
