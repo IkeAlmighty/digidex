e6838b5bd6e952bb1faa693c631ef1dc7626639e
ikealmighty <isaacyates7@gmail.com>
2025-02-06 14:32:25 -0600

    created SubscriptionComponent

--- client/src/pages/ManageSubsPage.jsx
+++ client/src/pages/ManageSubsPage.jsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from "react"
+import SubscriptionComponent from "../components/SubscriptionComponent";
+
 function ManageSubsPage() {
+
+    const styles = {
+        addButton: {
+            padding: "0.3rem"
+        }
+    }
+
+    const [subscriptions, setSubscriptions] = useState(["ike/work"]);
+    const [newSubscription, setNewSubscription] = useState("");
+
+    useEffect(loadSubscriptions, []);
+
+    function loadSubscriptions() {
+
+    }
+
+    function handleRemoveSubscription(tag) {
+        // console.log(e.target.getAttribute('tag'));
+        setSubscriptions(subscriptions.filter(s => s !== tag))
+    }
+
+    function handleAddSubscription() {
+
+        // update ui
+        setSubscriptions([...subscriptions, newSubscription]);
+
+        // TODO: update backend
+
+        // TODO: if backend fails, remove the newly addedSubscription and send toast error:
+    }
 
     return (
         <>
-            <div>Manage Subscriptions Page</div>
+            <div>
+                <h2>Manage Subscriptions Page</h2>
+            </div>
+            {subscriptions.map(tag => <SubscriptionComponent tag={tag} onTrash={() => handleRemoveSubscription(tag)} />)}
+
+            <hr />
+            <input style={styles.addButton} type="text" onChange={(e) => setNewSubscription(e.target.value)} />
+            <button onClick={handleAddSubscription}>+</button>
         </>
     )
 }
