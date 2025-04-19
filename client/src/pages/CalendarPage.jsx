e0f300911c3bf981f82e8b7619b27f6b3e374bea
ikealmighty <isaacyates7@gmail.com>
2025-04-19 00:41:00 -0500

    added mobile friendly styling and hook for fetching event data

--- client/src/pages/CalendarPage.jsx
+++ client/src/pages/CalendarPage.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TimeSpanButton from "../components/calendar/TimeSpanButton";
 import ThreeDayCalColumns from "../components/calendar/ThreeDayCalColumns";
+import { getEvents } from "../api/events";
 
 function CalendarPage() {
   const [timeSpanSelected, setTimeSpanSelected] = useState("3 Day");
   const [today, setToday] = useState(new Date());
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setEvents(await getEvents(today));
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
 
   return (
     <div className="m-3">
-      <div className="flex">
-        <h1 className="w-1/2 p-2">Your Calendar</h1>
-        <div className="w-1/2 p-2 flex items-end justify-content">
+      <div>
+        <h1 className="block p-2">Your Calendar</h1>
+        <div className="py-2 flex items-end justify-content">
           {["1 Day", "3 Day", "1 Week", "1 Month"].map((text, index) => (
             <TimeSpanButton
               key={`timespan-${index}`}
@@ -23,7 +35,7 @@ function CalendarPage() {
         </div>
       </div>
       {timeSpanSelected === "3 Day" && (
-        <ThreeDayCalColumns firstDate={today} events={[]} />
+        <ThreeDayCalColumns firstDate={today} events={events} />
       )}
     </div>
   );
