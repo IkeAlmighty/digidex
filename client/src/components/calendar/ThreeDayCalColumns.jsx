e22dfc9845e25c15b1b4c5beddad075c2dbba3c7
ikealmighty <isaacyates7@gmail.com>
2025-04-19 00:39:26 -0500

    refractored with EventColumn component

--- client/src/components/calendar/ThreeDayCalColumns.jsx
+++ client/src/components/calendar/ThreeDayCalColumns.jsx
@@ -1,18 +1,15 @@
-export default function ThreeDayCalColumns({ firstDate, events }) {
-  function findDate(startDate, offset) {
-    const newDate = new Date(startDate);
-    newDate.setDate(newDate.getDate() + offset);
-    return newDate;
-  }
+import { findDateFrom } from "../../utils/dates";
+import EventColumn from "../EventColumn";
 
-  const dates = [firstDate, findDate(firstDate, 1), findDate(firstDate, 2)];
+export default function ThreeDayCalColumns({ firstDate, events }) {
+
+  const dates = [firstDate, findDateFrom(firstDate, 1), findDateFrom(firstDate, 2)];
 
   return (
     <div className="h-[calc(100vh-6rem)] w-[calc(100vw-1.5rem)] fixed flex">
-      {dates.map((date) => (
-        <div className="mx-1 p-1 w-1/3 h-full bg-blue-500">
-          <div>{date.toDateString()}</div>
-          <div></div>
+      {dates.map((date, index) => (
+        <div className="mx-1 p-1 w-1/3 h-full bg-blue-500" key={`col-${index}`}>
+          <EventColumn date={date} events={events} />
         </div>
       ))}
     </div>
