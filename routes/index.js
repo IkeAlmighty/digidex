f323aa19fd6d322038637dd0b78c077ef6358668
ikealmighty <isaacyates7@gmail.com>
2025-04-27 12:32:29 -0500

    added accountRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
-import eventRoutes from './api/events.js';
-import tagRoutes from './api/tags.js';
-import userRoutes from './api/users.js'
-import express from 'express';
+import eventRoutes from "./api/events.js";
+import tagRoutes from "./api/tags.js";
+import userRoutes from "./api/users.js";
+import accountRoutes from "./api/accounts.js";
+import express from "express";
 
-import auth from '../utils/auth.js'
+import auth from "../utils/auth.js";
 
 const router = express.Router();
 
-router.use('/api/events', auth, eventRoutes);
-router.use('/api/tags', auth, tagRoutes);
-router.use('/api/users', auth, userRoutes);
+router.use("/api/events", auth, eventRoutes);
+router.use("/api/tags", auth, tagRoutes);
+router.use("/api/users", auth, userRoutes);
+router.use("/api/account", accountRoutes);
 
 export default router;
