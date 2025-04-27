cbf4718c5ca500f62e89a6a1f498cd77f5f33a7e
ikealmighty <isaacyates7@gmail.com>
2025-04-27 16:03:16 -0500

    refractored auth method to authenticateMiddleware

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,13 +4,13 @@ import userRoutes from "./api/users.js";
 import accountRoutes from "./api/accounts.js";
 import express from "express";
 
-import auth from "../utils/auth.js";
+import { authenticateMiddleware } from "../utils/auth.js";
 
 const router = express.Router();
 
-router.use("/api/events", auth, eventRoutes);
-router.use("/api/tags", auth, tagRoutes);
-router.use("/api/users", auth, userRoutes);
+router.use("/api/events", authenticateMiddleware, eventRoutes);
+router.use("/api/tags", authenticateMiddleware, tagRoutes);
+router.use("/api/users", authenticateMiddleware, userRoutes);
 router.use("/api/account", accountRoutes);
 
 export default router;
