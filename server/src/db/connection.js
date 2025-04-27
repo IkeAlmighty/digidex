8999626d2a1588c18f3885e2b8928db7da5501ba
ikealmighty <isaacyates7@gmail.com>
2025-04-27 16:02:49 -0500

    added models and mongoose integration

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,37 +1,8 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-import 'dotenv/config';
-// Replace the placeholder with your Atlas connection string
-const uri = process.env.MONGO_URI || 'mongodb://localhost:27017';
-const dbName = process.env.DB_NAME || 'polysched';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/polysched";
+mongoose.connect(uri);
 
-let db = undefined;
-
-// singleton since we only want one connection per process
-async function connectToDatabase() {
-    try {
-        if (!db) {
-            await client.connect();
-
-            db = await client.db(dbName);
-        }
-
-        return db;
-    } catch (err) {
-        console.dir(err);
-    }
-}
-
-async function closeConnection() {
-    await client.close()
-}
-
-export { connectToDatabase, closeConnection };
+export default mongoose.connection;
---
+++ server/src/models/index.js
@@ -0,0 +1,5 @@
+import User from "./User.js";
+import Event from "./Event.js";
+import Tag from "./Tag.js";
+
+export { User, Event, Tag };
