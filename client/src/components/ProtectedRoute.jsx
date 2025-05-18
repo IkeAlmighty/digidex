50347bcb21b0cd579116b71cf93492c77eb56b62
ikealmighty <isaacyates7@gmail.com>
2025-05-18 14:17:54 -0500

    added protected routes to frontend

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router";
+import React, { useEffect, useState } from "react";
+import { me } from "../api/accounts";
+
+export default function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null); // null == loading
+
+  useEffect(() => {
+    (async function () {
+      const response = await me();
+
+      if (response.ok) {
+        setUser(await response.json());
+      } else {
+        navigate("/login");
+      }
+    })();
+  }, []);
+
+  if (user === null) return <div>...Loading</div>;
+
+  // inject user into valid elements:
+  return React.Children.map(children, (child) => {
+    if (React.isValidElement(child)) {
+      return React.cloneElement(child, { user });
+    }
+
+    return child;
+  });
+}
