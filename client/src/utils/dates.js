723eafed7bfa29ef1eb7c2109c8b476e76da6719
ikealmighty <isaacyates7@gmail.com>
2025-05-18 12:32:44 -0500

    added getMidnight function to get midnight of the day of any ms timestamp

--- client/src/utils/dates.js
+++ client/src/utils/dates.js
@@ -1,5 +1,4 @@
-
-// Finds date offset by a number of days. 
+// Finds date offset by a number of days.
 // Use negative numbers to find dates before the startDate
 export function findDateFrom(startDate, offset) {
   const newDate = new Date(startDate);
@@ -7,10 +6,20 @@ export function findDateFrom(startDate, offset) {
   return newDate;
 }
 
-// Finds date offset by a number of minutes. 
+// Finds date offset by a number of minutes.
 // Use negative numbers to find times before the startDate
 export function findMinutesFrom(startDate, offset) {
-    const newDate = new Date(startDate);
-    newDate.setMinutes(newDate.getMinutes() + offset);
-    return newDate;
+  const newDate = new Date(startDate);
+  newDate.setMinutes(newDate.getMinutes() + offset);
+  return newDate;
+}
+
+export function getMidnight(epochMs) {
+  let date = new Date(epochMs);
+  date.setUTCHours(0);
+  date.setUTCMinutes(0);
+  date.setUTCSeconds(0);
+  date.setUTCMilliseconds(0);
+
+  return date;
 }
