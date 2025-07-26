405c8ab7827aaff66c2ddd60e0398b0c7755fdff
ikealmighty <isaacyates7@gmail.com>
2025-07-26 15:42:39 -0500

    changed the default db namne

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/polysched";
+const uri =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/people-tracker";
 mongoose.connect(uri);
 
 export default mongoose.connection;
