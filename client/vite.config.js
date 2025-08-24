fbf23eb7b752f5dd5f5a9d32390d2c54842b9b11
ikealmighty <isaacyates7@gmail.com>
2025-08-24 12:38:33 -0500

    added pwa plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,53 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+// you can copy the base structure of manifest object.
+const manifestForPlugIn = {
+  registerType: "prompt",
+  manifest: {
+    name: "Digidex",
+    short_name: "digidex",
+    description: "A digital rolodex to replace your contacts app.",
+    icons: [
+      {
+        src: "/favicon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/favicon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/favicon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/favicon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugIn)],
   server: {
     proxy: {
       "/api": {
