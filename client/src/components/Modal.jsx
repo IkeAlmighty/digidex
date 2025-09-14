7936da26d91ec71ec8520ae20fc4adf1d75dd164
ikealmighty <isaacyates7@gmail.com>
2025-09-14 18:46:21 -0500

    set left-0 so that modal appears in the right place on desktop

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,6 +1,6 @@
 export default function Modal({ children }) {
   return (
-    <div className="top-15 fixed w-screen h-screen bg-white">
+    <div className="top-15 left-0 fixed w-screen h-screen bg-white">
       <div className="my-20">{children}</div>
     </div>
   );
